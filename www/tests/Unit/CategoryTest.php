0eab109fd42049205f717cab93822eff4761a4a3
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-23 02:15:59 -0300

    test: criados os testes unitarios de category

---
+++ www/tests/Unit/CategoryTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Category;
+use PHPUnit\Framework\TestCase;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CategoryTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_fillableAttributes()
+    {
+        $fillable = ['name', 'description', 'is_active'];
+        $category = new Category();
+
+        $this->assertEquals(
+            $fillable,
+            $category->getFillable()
+        );
+    }
+
+    public function test_ifUseTraits()
+    {
+        $traits = [
+            HasFactory::class,
+            SoftDeletes::class,
+            Uuid::class
+        ];
+
+        $categoryTraits = array_keys(class_uses(Category::class));
+
+        $this->assertEquals(
+            $traits,
+            $categoryTraits
+        );
+    }
+
+    public function test_datesAttributes()
+    {
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
+        $category = new Category();
+        $categoryDates = $category->getDates();
+
+        foreach($dates as $date) {
+            $this->assertContains($date, $categoryDates);
+        }
+
+        $this->assertCount(count($dates), $categoryDates);
+    }
+
+    public function test_casts()
+    {
+        $casts = [
+            'id' => 'string',
+            'deleted_at' => 'datetime'
+        ];
+        $category = new Category();
+
+        $this->assertEquals($casts, $category->getCasts());
+    }
+
+    public function test_incrementing()
+    {
+        $category = new Category();
+
+        $this->assertFalse($category->getIncrementing());
+    }
+}

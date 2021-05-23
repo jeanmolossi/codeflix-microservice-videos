fe8ea8a2e56538f617df5f3a47cae7a10de795dc
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-23 01:13:51 -0300

    chore: criado e implementado controller de genres

---
+++ www/app/Http/Controllers/Api/GenreController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Genre;
+use Illuminate\Http\Request;
+
+class GenreController extends Controller
+{
+
+    private $rules = [
+        'name' => 'required|max:255'
+    ];
+
+    public function index()
+    {
+        return Genre::all();
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, $this->rules);
+
+        return Genre::create($request->all());
+    }
+
+    public function show(Genre $genre)
+    {
+        return $genre;
+    }
+
+    public function update(Request $request, Genre $genre)
+    {
+        $this->validate($request, $this->rules);
+
+        return $genre->update($request->all());
+    }
+
+    public function destroy(Genre $genre)
+    {
+        $genre->delete();
+
+        return response()->noContent();
+    }
+}

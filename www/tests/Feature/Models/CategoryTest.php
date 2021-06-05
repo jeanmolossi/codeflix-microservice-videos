8b2ecf7540f95f61801ac4273c9afead9f9089f6
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-06-05 11:02:07 -0300

    feat: criado teste de integracao para category

---
+++ www/tests/Feature/Models/CategoryTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}

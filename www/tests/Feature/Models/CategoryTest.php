c76c89a8e7d296414b7bb86ce524e10970fbed68
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-06-05 16:55:00 -0300

    test: criado teste de integracao de category

--- www/tests/Feature/Models/CategoryTest.php
+++ www/tests/Feature/Models/CategoryTest.php
@@ -2,23 +2,46 @@
 
 namespace Tests\Feature\Models;
 
+use App\Models\Category;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class CategoryTest extends TestCase
-{
+class CategoryTest extends TestCase {
     use DatabaseMigrations;
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    public function test_List() {
+        Category::factory(1)->create();
+
+        $categories = Category::all();
+        $this->assertCount(1, $categories);
+
+        $categoryKeys = array_keys($categories->first()->getAttributes());
+
+        $this->assertEqualsCanonicalizing(
+            [
+                'id',
+                'name',
+                'description',
+                'is_active',
+                'created_at',
+                'updated_at',
+                'deleted_at'
+            ],
+            $categoryKeys
+        );
+    }
+
+    public function test_create() {
+        $category = Category::create(
+            ['name' => 'Test']
+        );
+
+        $category->refresh();
+
+        $this->assertEquals("Test", $category->name);
+        $this->assertNull($category->description);
+        $this->assertTrue($category->is_active);
     }
 }

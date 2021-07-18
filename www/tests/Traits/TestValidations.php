93a6f97484fd8eaa9ededc1b3c7db149d4412ee8
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-07-18 10:57:55 -0300

    test: criada abstracao de validacao para testes

--- www/tests/Traits/TestValidations.php
+++ www/tests/Traits/TestValidations.php
@@ -6,12 +6,41 @@ use Illuminate\Support\Facades\Lang;
 use Illuminate\Testing\TestResponse;
 
 trait TestValidations {
+    protected abstract function model();
 
-    protected function assertInvalidationFields (
+    protected abstract function routeStore();
+
+    protected abstract function routeUpdate();
+
+    protected function assertInvalidationInStoreAction(array $data, string $rule, $ruleParams = []) {
+        $response = $this->json(
+            "POST",
+            $this->routeStore(),
+            $data
+        );
+
+        $fields = array_keys($data);
+
+        $this->assertInvalidationFields($response, $fields, $rule, $ruleParams);
+    }
+
+    protected function assertInvalidationInUpdateAction(array $data, string $rule, $ruleParams = []) {
+        $response = $this->json(
+            "PUT",
+            $this->routeUpdate(),
+            $data
+        );
+
+        $fields = array_keys($data);
+
+        $this->assertInvalidationFields($response, $fields, $rule, $ruleParams);
+    }
+
+    protected function assertInvalidationFields(
         TestResponse $response,
-        array $fields,
-        string $rule,
-        array $ruleParams = []
+        array        $fields,
+        string       $rule,
+        array        $ruleParams = []
     ) {
         $response
             ->assertStatus(422)

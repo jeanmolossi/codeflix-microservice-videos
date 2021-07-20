6015c2a6c26854b182f588d82e683e5a887934a7
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-07-19 21:05:48 -0300

    refactor: adicionados relacionamentos de categorias e generos

--- www/app/Http/Controllers/Api/VideoController.php
+++ www/app/Http/Controllers/Api/VideoController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\Video;
+use DB;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Throwable;
 
-class VideoController extends BasicCrudController
-{
+class VideoController extends BasicCrudController {
     private $rules;
 
     public function __construct() {
@@ -16,20 +17,69 @@ class VideoController extends BasicCrudController
             'description' => 'required',
             'year_launched' => 'required|date_format:Y',
             'opened' => 'boolean',
-            'rating' => 'required|in:'. implode(',', Video::RATING_LIST),
+            'rating' => 'required|in:' . implode(',', Video::RATING_LIST),
             'duration' => 'required|integer',
+            'categories_id' => 'required|array|exists:categories,id',
+            'genres_id' => 'required|array|exists:genres,id'
         ];
+    }
+
+    /**
+     * @throws Throwable
+     * @throws ValidationException
+     */
+    public function store(Request $request): Video {
+        $validatedData = $this->validate($request, $this->rulesStore());
+
+        $self = $this;
+
+        /** @var Video $obj */
+        $obj = DB::transaction(function () use ($request, $validatedData, $self) {
+            $obj = $this->model()::create($validatedData);
+            $self->handleRelations($obj, $request);
+            return $obj;
+        });
+        $obj->refresh();
+
+        return $obj;
+    }
+
+    /**
+     * @throws Throwable
+     * @throws ValidationException
+     */
+    public function update(Request $request, $id) {
+        $obj = $this->findOrFail($id);
+
+        $validatedData = $this->validate($request, $this->rulesUpdate());
+
+        $self = $this;
+
+        return DB::transaction(function () use ($request, $validatedData, $self, $obj) {
+            $obj->update($validatedData);
+            $self->handleRelations($obj, $request);
+
+            return $obj;
+        });
+    }
+
+    protected function handleRelations(Video $video, Request $request) {
+        $video->categories()->sync($request->get('categories_id'));
+        $video->genres()->sync($request->get('genres_id'));
     }
 
     protected function model(): string {
         return Video::class;
     }
 
-    protected function rulesStore(): array {
+    protected
+    function rulesStore(): array {
         return $this->rules;
     }
 
-    protected function rulesUpdate(): array {
+    protected
+    function rulesUpdate(): array {
         return $this->rules;
     }
 }
+

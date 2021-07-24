865c3980b064f0f15566455df5b6197103971725
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-07-24 13:05:35 -0300

    refactor: adicionados regra de busca de relacionamentos na lixeira

--- www/app/Models/Video.php
+++ www/app/Models/Video.php
@@ -35,11 +35,11 @@ class Video extends Model {
     public $incrementing = false;
 
     public function categories(): BelongsToMany {
-        return $this->belongsToMany(Category::class);
+        return $this->belongsToMany(Category::class)->withTrashed();
     }
 
     public function genres(): BelongsToMany {
-        return $this->belongsToMany(Genre::class);
+        return $this->belongsToMany(Genre::class)->withTrashed();
     }
 }
 

25c89b8e747a5dc77c15604dc1cb75b023b7ba07
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-07-24 13:07:12 -0300

    feat: criada a regra de validacao para relacionamentos de generos

---
+++ www/app/Rules/GenresHasCategoriesRule.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Rules;
+
+use DB;
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Collection;
+
+class GenresHasCategoriesRule implements Rule {
+    private $categoriesId;
+
+    private $genresId;
+
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct(array $categoriesId) {
+        $this->categoriesId = array_unique($categoriesId);
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param string $attribute
+     * @param mixed $value
+     * @return bool
+     */
+    public function passes($attribute, $value): bool {
+        $this->genresId = array_unique($value);
+
+        if(!count($this->genresId) || !count($this->categoriesId)) {
+            return false;
+        }
+
+        $categoriesFound = [];
+
+        foreach ($this->genresId as $genreId) {
+            $rows = $this->getRows($genreId);
+
+            if(!$rows->count()) {
+                return false;
+            }
+
+            array_push($categoriesFound, ...$rows->pluck('category_id')->toArray());
+        }
+
+        if(count($categoriesFound) !== count($this->categoriesId)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function getRows($genreId): Collection {
+        return DB::table('category_genre')
+            ->where('genre_id', $genreId)
+            ->whereIn('category_id', $this->categoriesId)
+            ->get();
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message() {
+        return 'A genre ID must be related at least a category ID';
+    }
+}

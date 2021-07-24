bba01a4e4b296a6b0bf2edb52ace2f111c790858
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-07-24 20:24:11 -0300

    feat: adicionada custom rule de relacionamento de genero e categoria

--- www/app/Http/Controllers/Api/VideoController.php
+++ www/app/Http/Controllers/Api/VideoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Video;
+use App\Rules\GenresHasCategoriesRule;
 use DB;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
@@ -20,7 +21,11 @@ class VideoController extends BasicCrudController {
             'rating' => 'required|in:' . implode(',', Video::RATING_LIST),
             'duration' => 'required|integer',
             'categories_id' => 'required|array|exists:categories,id,deleted_at,NULL',
-            'genres_id' => 'required|array|exists:genres,id,deleted_at,NULL'
+            'genres_id' => [
+                'required',
+                'array',
+                'exists:genres,id,deleted_at,NULL'
+            ]
         ];
     }
 
@@ -29,6 +34,9 @@ class VideoController extends BasicCrudController {
      * @throws ValidationException
      */
     public function store(Request $request): Video {
+        $this->addRuleIfGenreHasCategories($request);
+
+
         $validatedData = $this->validate($request, $this->rulesStore());
 
         $self = $this;
@@ -51,6 +59,8 @@ class VideoController extends BasicCrudController {
     public function update(Request $request, $id) {
         $obj = $this->findOrFail($id);
 
+        $this->addRuleIfGenreHasCategories($request);
+
         $validatedData = $this->validate($request, $this->rulesUpdate());
 
         $self = $this;
@@ -61,6 +71,16 @@ class VideoController extends BasicCrudController {
 
             return $obj;
         });
+    }
+
+    protected function addRuleIfGenreHasCategories(Request $request) {
+        $categoriesId = $request->get('categories_id');
+
+        $categoriesId = is_array($categoriesId) ? $categoriesId : [];
+
+        $this->rules['genres_id'][] = new GenresHasCategoriesRule(
+            $categoriesId
+        );
     }
 
     protected function handleRelations(Video $video, Request $request) {

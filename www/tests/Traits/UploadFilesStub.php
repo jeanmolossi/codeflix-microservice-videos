f5ff9fc8b2c6d03705f4a5bcff062f8a3a37fca6
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-07-25 18:59:15 -0300

    test: adicionado fileFields

--- www/tests/Traits/UploadFilesStub.php
+++ www/tests/Traits/UploadFilesStub.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class UploadFilesStub extends Model {
     use UploadFiles;
 
+    public static $fileFields = ['file_1', 'file_2'];
+
     protected function uploadDir() {
         return "1";
     }

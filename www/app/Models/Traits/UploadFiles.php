c9a835211f0422c48b0baee5e93851ca5aa5dfcb
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-07-24 23:12:42 -0300

    feat: criada trait de upload de arquivos

---
+++ www/app/Models/Traits/UploadFiles.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models\Traits;
+
+use Illuminate\Http\UploadedFile;
+use Storage;
+
+trait UploadFiles {
+
+    protected abstract function uploadDir();
+
+    /**
+     * @param UploadedFile[] $files
+     */
+    public function uploadFiles(array $files) {
+        foreach ($files as $file) {
+            $this->uploadFile($file);
+        }
+    }
+
+    public function uploadFile(UploadedFile $file) {
+        $file->store(
+            $this->uploadDir()
+        );
+    }
+
+    /**
+     * @param UploadedFile[] $files
+     */
+    public function deleteFiles(array $files) {
+        foreach ($files as $file) {
+            $this->deleteFile($file);
+        }
+    }
+
+    /**
+     * @param string | UploadedFile $file
+     */
+    public function deleteFile($file) {
+        $filename = $file instanceof UploadedFile ? $file->hashName() : $file;
+        Storage::delete("{$this->uploadDir()}/{$filename}");
+    }
+}

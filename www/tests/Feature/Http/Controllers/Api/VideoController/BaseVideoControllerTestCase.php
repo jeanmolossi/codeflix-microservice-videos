b991df53d84b45ea53a1ca037b1ebd4b12374678
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-07-31 01:45:35 -0300

    feat: adicionados relacionamentos de categories_id e genres_id

--- www/tests/Feature/Http/Controllers/Api/VideoController/BaseVideoControllerTestCase.php
+++ www/tests/Feature/Http/Controllers/Api/VideoController/BaseVideoControllerTestCase.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\Http\Controllers\Api\VideoController;
 
+use App\Models\Category;
+use App\Models\Genre;
 use App\Models\Video;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -19,12 +21,20 @@ abstract class BaseVideoControllerTestCase extends TestCase
 
         $this->video = Video::factory()->create(['opened' => false]);
 
+        $category = Category::factory()->create();
+
+        /** @var Genre $genre */
+        $genre = Genre::factory()->create();
+        $genre->categories()->sync($category->id);
+
         $this->sendData = [
             'title' => 'title_test',
             'description' => 'description_test',
             'year_launched' => 2010,
             'rating' => Video::RATING_LIST[0],
-            'duration' => 90
+            'duration' => 90,
+            'categories_id' => [$category->id],
+            'genres_id' => [$genre->id],
         ];
     }
 

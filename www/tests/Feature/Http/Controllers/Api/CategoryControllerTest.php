272852c8d6611f69920afefb0b3bd016bc12a73d
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-07-31 14:42:04 -0300

    test: adaptados testes para resource

--- www/tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
+++ www/tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
@@ -36,7 +36,20 @@ class CategoryControllerTest extends TestCase {
 
         $response
             ->assertStatus(200)
-            ->assertJson([$this->category->toArray()]);
+            ->assertJson([
+                'meta' => ['per_page' => 15]
+            ])
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => $this->serializedFields
+                ],
+                'links' => [],
+                'meta' => [],
+            ]);
+
+        $resource = CategoryResource::collection(collect([$this->category]));
+
+        $this->assertResource($response, $resource);
     }
 
     public function test_Show() {

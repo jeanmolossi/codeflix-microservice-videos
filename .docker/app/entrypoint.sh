4c4fa44eb10a152b221579f1c11e89148e511a37
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-07-31 17:34:01 -0300

    feat: alterada interacao de arquivos de environment no entrypoint.sh

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -2,8 +2,15 @@
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 chown -R www-data:www-data .
-cp .env.example .env
-cp .env.testing.example .env.testing
+
+if [ ! -f ".env" ]; then
+    cp .env.example .env
+fi
+
+if [ ! -f ".env.testing" ]; then
+    cp .env.testing.example .env.testing
+fi
+
 composer install
 php artisan key:generate
 php artisan migrate

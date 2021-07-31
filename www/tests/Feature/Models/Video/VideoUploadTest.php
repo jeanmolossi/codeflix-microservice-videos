e71ef99d0c22ef7f7ce34a565f7fa924e078b326
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-07-31 02:01:45 -0300

    test: testes de update e rollback e de urls

--- www/tests/Feature/Models/Video/VideoUploadTest.php
+++ www/tests/Feature/Models/Video/VideoUploadTest.php
@@ -46,4 +46,78 @@ class VideoUploadTest extends BaseVideoTestCase {
         $this->assertTrue($hasError);
     }
 
+    public function test_UploadWithFiles() {
+        Storage::fake();
+
+        $video = Video::factory()->create();
+
+        $thumbFile = UploadedFile::fake()->image('thumb.jpg');
+        $videoFile = UploadedFile::fake()->create('video.mp4');
+        $video->update($this->data + [
+                'thumb_file' => $thumbFile,
+                'video_file' => $videoFile
+            ]);
+
+        Storage::assertExists("{$video->id}/{$video->thumb_file}");
+        Storage::assertExists("{$video->id}/{$video->video_file}");
+
+        $newVideoFile = UploadedFile::fake()->image("video.mp4");
+        $video->update($this->data + [
+                'video_file' => $newVideoFile
+            ]);
+
+        Storage::assertExists("{$video->id}/{$thumbFile->hashName()}");
+        Storage::assertExists("{$video->id}/{$newVideoFile->hashName()}");
+        Storage::assertMissing("{$video->id}/{$videoFile->hashName()}");
+    }
+
+    public function test_UpdateIfRollbackFiles() {
+        Storage::fake();
+
+        $video = Video::factory()->create();
+        Event::listen(TransactionCommitted::class, function () {
+            throw new TestException();
+        });
+        $hasError = false;
+
+        try {
+            $video->update(
+                $this->data + [
+                    'thumb_file' => UploadedFile::fake()->image('thumb.jpg'),
+                    'video_file' => UploadedFile::fake()->create('video.mp4')
+                ]
+            );
+        } catch (TestException $e) {
+            $this->assertCount(0, Storage::allFiles());
+            $hasError = true;
+        }
+
+        $this->assertTrue($hasError);
+    }
+
+    public function  test_FileUrlsWithLocalDriver() {
+        $fileFields = [];
+
+        foreach (Video::$fileFields as $field) {
+            $fileFields[$field] = "$field.test";
+        }
+
+        $video = Video::factory()->create($fileFields);
+        $localDriver = config('filesystems.default');
+        $baseUrl = config('filesystems.disks.' . $localDriver)['url'];
+
+        foreach ($fileFields as $field => $value) {
+            $fileUrl = $video->{"{$field}_url"};
+            $this->assertEquals("{$baseUrl}/{$video->id}/{$value}", $fileUrl);
+        }
+    }
+
+    public function test_FileUrlsIfNullWhenFieldsAreNull() {
+        $video = Video::factory()->create();
+
+        foreach (Video::$fileFields as $field) {
+            $fileUrl = $video->{"{$field}_url"};
+            $this->assertNull($fileUrl);
+        }
+    }
 }

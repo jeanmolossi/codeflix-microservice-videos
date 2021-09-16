c9b04f036e79bd31d20c2929379ba4a7ceed9ea8
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-09-16 00:06:37 -0300

    feat: criado frontend da aplicacao

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@material-ui/core";
+import { Navbar } from "./components";
+import { BrowserRouter } from "react-router-dom";
+import { AppRouter } from "./routes/AppRouter";
+import { Breadcrumbs } from "./components/Breadcrumbs";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <BrowserRouter>
+            <Navbar/>
+            <Box paddingTop={ '70px' }>
+                <Breadcrumbs/>
+                <AppRouter/>
+            </Box>
+        </BrowserRouter>
     );
 }
 

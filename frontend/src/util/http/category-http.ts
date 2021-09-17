e69353d7d26443997ab8be683fc7204503aeca96
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-09-16 22:57:37 -0300

    feat: criado resource e category resource

---
+++ frontend/src/util/http/category-http.ts
@@ -0,0 +1,11 @@
+import { httpVideo } from "./index";
+import { HttpResource } from "./http-resource";
+
+export interface Category {
+    id: string;
+    name: string;
+    is_active: boolean;
+}
+
+export const categoryHttp = new HttpResource<Category, string>(httpVideo, 'categories')
+
---
+++ frontend/src/util/http/http-resource.ts
@@ -0,0 +1,31 @@
+import { AxiosInstance, AxiosResponse } from 'axios';
+
+type DataResource<T> = { data: T };
+type DataResponse<T> = AxiosResponse<DataResource<T>>;
+
+export class HttpResource<T = any, ID = any> {
+
+    constructor(protected http: AxiosInstance, protected resource: string) {
+    }
+
+    list(): Promise<DataResponse<T[]>> {
+        return this.http.get<DataResource<T[]>>(this.resource)
+    }
+
+    get(id: ID): Promise<DataResponse<T>> {
+        return this.http.get<DataResource<T>>(`${ this.resource }/${ id }`)
+    }
+
+    create<D = any>(data: D): Promise<DataResponse<T>> {
+        return this.http.post<DataResource<T>>(this.resource, data)
+    }
+
+    update<D = any>(id: ID, data: D): Promise<DataResponse<T>> {
+        return this.http.put<DataResource<T>>(`${ this.resource }/${ id }`, data)
+    }
+
+    delete(id: ID): Promise<DataResponse<T>> {
+        return this.http.delete<DataResource<T>>(`${ this.resource }/${ id }`)
+    }
+
+}

c44c5b843139a00a812dcdc772a6dd0751628f22
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-09-19 01:49:59 -0300

    feat: implementado react-hook-form

--- frontend/src/pages/category/components/Form.tsx
+++ frontend/src/pages/category/components/Form.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import { TextField, Checkbox, Box, Button, ButtonProps, makeStyles } from "@material-ui/core";
+import React, { BaseSyntheticEvent, useCallback } from 'react';
+import { Box, Button, ButtonProps, Checkbox, makeStyles, TextField } from "@material-ui/core";
+import { useForm } from "react-hook-form";
+import { Category, categoryHttp } from "../../../util/http/category-http";
 
 type FormProps = {}
+
+type FormFields = Omit<Category, 'id'>;
 
 const useStyles = makeStyles(theme => ({
     submit: {
@@ -9,7 +13,7 @@ const useStyles = makeStyles(theme => ({
     }
 }))
 
-export const Form = ({}: FormProps) => {
+export const Form = ({ ..._ }: FormProps) => {
     const classes = useStyles();
 
     const buttonProps: ButtonProps = {
@@ -17,13 +21,31 @@ export const Form = ({}: FormProps) => {
         variant: 'outlined',
     }
 
+    const { register, handleSubmit, getValues } = useForm<FormFields>({
+        defaultValues: {
+            is_active: true
+        }
+    });
+
+    const onSubmit = useCallback((formData: FormFields, _: BaseSyntheticEvent | undefined) => {
+        categoryHttp.create(formData)
+            .then((response) =>
+                console.log(response.data.data)
+            )
+    }, []);
+
+    const onSubmitOnly = () => onSubmit(getValues(), undefined)
+
     return (
-        <form>
+        <form onSubmit={ handleSubmit(onSubmit) }>
             <TextField
                 name={ 'name' }
                 label={ 'Nome' }
                 fullWidth
                 variant={ 'outlined' }
+                inputProps={ {
+                    ...register('name')
+                } }
             />
 
             <TextField
@@ -34,17 +56,24 @@ export const Form = ({}: FormProps) => {
                 fullWidth
                 variant={ 'outlined' }
                 margin={ 'normal' }
+                inputProps={ {
+                    ...register('description')
+                } }
             />
 
             <Checkbox
                 name={ 'is_active' }
                 aria-label={ 'Ativo ?' }
                 id={ 'is_active' }
+                inputProps={ {
+                    ...register('is_active')
+                } }
+                defaultChecked
             />
             <label htmlFor={ 'is_active' }>Ativo ?</label>
 
             <Box dir={ 'rtl' }>
-                <Button { ...buttonProps }>Salvar</Button>
+                <Button { ...buttonProps } onClick={ onSubmitOnly }>Salvar</Button>
                 <Button { ...buttonProps } type={ 'submit' }>Salvar e continuar editando</Button>
             </Box>
         </form>

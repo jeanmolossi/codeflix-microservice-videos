b00aa1fdad8111e00be5d090a46f5ed318dd12c9
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-09-19 16:04:10 -0300

    style: adicionada coloracao de botao

--- frontend/src/pages/genre/ListGenres.tsx
+++ frontend/src/pages/genre/ListGenres.tsx
@@ -14,6 +14,7 @@ export const ListGenres = () => {
                     size={ 'small' }
                     component={ Link }
                     to={ '/generos/criar' }
+                    color={ 'secondary' }
                 >
                     <Add />
                 </Fab>

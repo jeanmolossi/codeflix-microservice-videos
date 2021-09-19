fbeae6cdf01f8029689da081f8a945add73a8d88
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-09-19 01:52:27 -0300

    feat: criada pagina de create cast-member

---
+++ frontend/src/pages/cast-members/CreateCastMember.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Page } from "../../components";
+import { Form } from "./components";
+
+export const CreateCastMember = () => {
+    return (
+        <Page title={ 'Criar membro de elenco' }>
+            <Form />
+        </Page>
+    )
+}

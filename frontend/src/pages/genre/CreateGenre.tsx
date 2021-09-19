7834c64477ad91a20dfd4c944ec6241c93fa4cfa
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-09-19 01:53:52 -0300

    feat: criada pagina de create genre

---
+++ frontend/src/pages/genre/CreateGenre.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Page } from "../../components";
+import { Form } from "./components";
+
+export const CreateGenre = () => {
+    return (
+        <Page title={ 'Criar gênero' }>
+            <Form />
+        </Page>
+    )
+}

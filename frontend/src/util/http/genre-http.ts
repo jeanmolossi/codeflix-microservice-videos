1a3feaeb13809e42b6aa6a1d283bae6d6cbcf01d
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-09-22 23:34:14 -0300

    refactor: adicionada categories a interface de Genre

--- frontend/src/util/http/genre-http.ts
+++ frontend/src/util/http/genre-http.ts
@@ -1,11 +1,13 @@
 import { HttpResource } from "./http-resource";
 import { httpVideo } from "./index";
+import { Category } from "./category-http";
 
 export interface Genre {
     id: string;
     name: string;
     is_active: boolean;
     categories_id: string[];
+    categories?: Category[];
 }
 
 export const genreHttp = new HttpResource<Genre>(

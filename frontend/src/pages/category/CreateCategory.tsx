32c758f2af7a85f5f05f9f20d5935afaab5e51d8
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-09-22 23:05:53 -0300

    feat: alterado label da página baseado no parameter da url

--- frontend/src/pages/category/CreateCategory.tsx
+++ frontend/src/pages/category/CreateCategory.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Page } from "../../components";
 import { Form } from "./components";
+import { useParams } from "react-router-dom";
 
 export const CreateCategory = () => {
+    const { id } = useParams<{ id?: string }>();
+
     return (
-        <Page title={ 'Criar categoria' }>
+        <Page title={ id ? 'Editar categoria' : 'Criar categoria' }>
             <Form />
         </Page>
     )

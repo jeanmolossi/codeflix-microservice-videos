e76c63cc72aa9833b82e2e06b77fe376abe93643
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-09-22 23:35:40 -0300

    feat: adicionada feature de redirect e de hydrate de campos

--- frontend/src/pages/genre/CreateGenre.tsx
+++ frontend/src/pages/genre/CreateGenre.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Page } from "../../components";
 import { Form } from "./components";
+import { useParams } from "react-router-dom";
 
 export const CreateGenre = () => {
+    const { id } = useParams<{ id?: string }>();
+
     return (
-        <Page title={ 'Criar gênero' }>
+        <Page title={ id ? 'Editar gênero' : 'Criar gênero' }>
             <Form />
         </Page>
     )
--- frontend/src/pages/genre/components/Form.tsx
+++ frontend/src/pages/genre/components/Form.tsx
@@ -1,10 +1,19 @@
 import React, { BaseSyntheticEvent, ChangeEvent, useCallback, useEffect, useState } from 'react';
-import { Box, Button, ButtonProps, Checkbox, makeStyles, MenuItem, TextField } from "@material-ui/core";
+import {
+    Box,
+    Button,
+    ButtonProps,
+    Checkbox,
+    FormControlLabel,
+    makeStyles,
+    MenuItem,
+    TextField
+} from "@material-ui/core";
 import { useForm } from "react-hook-form";
 import { Genre, genreHttp } from "../../../util/http/genre-http";
 import { Category, categoryHttp } from "../../../util/http/category-http";
-
-type FormProps = {}
+import { useHistory, useParams } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 type FormFields = Omit<Genre, 'id'>;
 
@@ -14,21 +23,31 @@ const useStyles = makeStyles(theme => ({
     }
 }))
 
-export const Form = ({ ..._ }: FormProps) => {
+export const Form = () => {
     const classes = useStyles();
+    const { id } = useParams<{ id?: string }>();
+    const history = useHistory();
+    const snackbar = useSnackbar();
+
+    const [ loading, setLoading ] = useState(false);
+    const [ categories, setCategories ] = useState<Category[]>([]);
+    const [ genre, setGenre ] = useState({} as Genre);
 
     const buttonProps: ButtonProps = {
         className: classes.submit,
         variant: 'contained',
-        color: 'secondary'
+        color: 'secondary',
+        disabled: loading,
     }
+
 
     const {
         register,
         handleSubmit,
         getValues,
         setValue,
-        watch
+        watch,
+        reset
     } = useForm<FormFields>({
         defaultValues: {
             is_active: true,
@@ -36,14 +55,40 @@ export const Form = ({ ..._ }: FormProps) => {
         }
     });
 
-    const [ categories, setCategories ] = useState<Category[]>([]);
-
-    const onSubmit = useCallback((formData: FormFields, _: BaseSyntheticEvent | undefined) => {
-        genreHttp.create(formData)
-            .then((response) =>
-                console.log(response.data.data)
+    const saveButtonsBehavior = useCallback((data: any, e?: BaseSyntheticEvent, id?: string) => {
+        if (!!e) {
+            (!!id
+                    ? history.replace(`/generos/${ data.data.id }/editar`)
+                    : history.push(`/generos/${ data.data.id }/editar`)
             )
-    }, []);
+            return;
+        }
+
+        history.push('/generos')
+    }, [ history ]);
+
+    const onSubmit = useCallback((formData: FormFields, e: BaseSyntheticEvent | undefined) => {
+        const http = genre.id
+            ? genreHttp.update(genre.id, formData)
+            : genreHttp.create(formData);
+
+        setLoading(true)
+        http
+            .then(({ data }) => {
+                snackbar.enqueueSnackbar(
+                    'Genero salvo com sucesso',
+                    { variant: "success" }
+                )
+                saveButtonsBehavior(data, e, id)
+            })
+            .catch((err) => {
+                snackbar.enqueueSnackbar(
+                    err.message,
+                    { variant: "error" }
+                )
+            })
+            .finally(() => setLoading(false))
+    }, [ genre.id, saveButtonsBehavior, id, snackbar ]);
 
     const onSubmitOnly = () => onSubmit(getValues(), undefined)
 
@@ -63,6 +108,25 @@ export const Form = ({ ..._ }: FormProps) => {
             .then(response => setCategories(response.data.data))
     }, []);
 
+    useEffect(() => {
+        if (id) {
+            genreHttp.get(id)
+                .then(({ data }) => {
+                    setGenre(data.data)
+
+                    const { categories } = data.data!;
+                    delete data.data.categories;
+
+                    const resetData = {
+                        ...data.data,
+                        categories_id: categories?.map(c => c.id)
+                    }
+
+                    reset(resetData)
+                })
+        }
+    }, [ id, reset ])
+
     return (
         <form onSubmit={ handleSubmit(onSubmit) }>
             <TextField
@@ -73,6 +137,10 @@ export const Form = ({ ..._ }: FormProps) => {
                 inputProps={ {
                     ...register('name')
                 } }
+                InputLabelProps={ {
+                    shrink: true
+                } }
+                disabled={ loading }
             />
 
             <TextField
@@ -86,6 +154,10 @@ export const Form = ({ ..._ }: FormProps) => {
                 fullWidth
                 SelectProps={ { multiple: true } }
                 onChange={ onChange }
+                disabled={ loading }
+                InputLabelProps={ {
+                    shrink: true
+                } }
             >
                 <MenuItem value="" disabled>
                     <em>Selecione categorias</em>
@@ -93,17 +165,22 @@ export const Form = ({ ..._ }: FormProps) => {
                 { categoriesOptionsHandler(categories) }
             </TextField>
 
-            <Checkbox
-                name={ 'is_active' }
-                aria-label={ 'Ativo ?' }
-                id={ 'is_active' }
-                inputProps={ {
-                    ...register('is_active')
-                } }
-                checked={ watch('is_active') }
-                color={ 'primary' }
+            <FormControlLabel
+                control={
+                    <Checkbox
+                        name={ 'is_active' }
+                        aria-label={ 'Ativo ?' }
+                        id={ 'is_active' }
+                        inputProps={ {
+                            ...register('is_active')
+                        } }
+                        checked={ watch('is_active') }
+                        color={ 'primary' }
+                    />
+                }
+                label={ 'Ativo ?' }
+                labelPlacement={ 'end' }
             />
-            <label htmlFor={ 'is_active' }>Ativo ?</label>
 
             <Box dir={ 'rtl' }>
                 <Button { ...buttonProps } onClick={ onSubmitOnly }>Salvar</Button>

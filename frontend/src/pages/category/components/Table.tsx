31f93f2bc0c56a2d32b87aa2af644cf16dc3c736
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-09-25 01:14:19 -0300

    feat: adicionada botao na coluna de acoes

--- frontend/src/pages/category/components/Table.tsx
+++ frontend/src/pages/category/components/Table.tsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { format, parseISO } from 'date-fns';
-
-import { categoryHttp } from "../../../util/http/category-http";
-import { BadgeNo, BadgeYes, Table as MyTable, TableColumn } from "../../../components";
-import { Category } from "../../../core/models";
 import { useSnackbar } from "notistack";
+import { IconButton, MuiThemeProvider } from "@material-ui/core";
+import { Edit as EditIcon } from "@material-ui/icons";
+import { categoryHttp } from "../../../util/http/category-http";
+import { BadgeNo, BadgeYes, makeActionStyles, Table as MyTable, TableColumn } from "../../../components";
+import { Category } from "../../../core/models";
+import { Link } from "react-router-dom";
+
 
 const columnsDefinition: TableColumn[] = [
     {
         name: 'id',
         label: 'ID',
+        width: '30%',
         options: {
             sort: false
         }
@@ -33,46 +37,63 @@ const columnsDefinition: TableColumn[] = [
         name: "created_at",
         label: "Criado em",
         options: {
-            customBodyRender: ( value ) => {
-                return <span>{ format( parseISO( value ), 'dd/MM/yyyy HH:ii' ) }</span>
+            customBodyRender: (value) => {
+                return <span>{ format(parseISO(value), 'dd/MM/yyyy HH:ii') }</span>
             }
         }
     },
     {
         name: "actions",
         label: "Ações",
+        options: {
+            sort: false,
+            customBodyRender: (value, tableMeta) => {
+                const [ id ] = tableMeta.rowData;
+                return (
+                    <IconButton
+                        color={ 'secondary' }
+                        component={ Link }
+                        to={ `/categorias/${ id }/editar` }
+                    >
+                        <EditIcon fontSize={ 'inherit' } />
+                    </IconButton>
+                )
+            }
+        }
     }
 ];
 
 export const Table = () => {
     const snackbar = useSnackbar();
-    const [ loading, setLoading ] = useState( false );
-    const [ data, setData ] = useState<Category[]>( [] );
+    const [ loading, setLoading ] = useState(false);
+    const [ data, setData ] = useState<Category[]>([]);
 
-    useEffect( () => {
-        setLoading( true )
+    useEffect(() => {
+        setLoading(true)
         categoryHttp.list()
-            .then( ( response ) =>
-                setData( response.data.data )
+            .then((response) =>
+                setData(response.data.data)
             )
-            .catch( ( err ) => {
+            .catch((err) => {
                 snackbar.enqueueSnackbar(
                     err.message,
                     { variant: "error" }
                 );
-            } )
+            })
             .finally( () => setLoading( false ) )
     }, [ snackbar ] );
 
     return (
-        <div>
-            <MyTable
-                columns={ columnsDefinition }
-                data={ data }
-                title={ 'Listagem de categorias' }
-                isLoading={ loading }
-                options={ { responsive: "simple" } }
-            />
-        </div>
+        <MuiThemeProvider theme={ makeActionStyles(columnsDefinition.length - 1) }>
+            <div>
+                <MyTable
+                    columns={ columnsDefinition }
+                    data={ data }
+                    title={ 'Listagem de categorias' }
+                    isLoading={ loading }
+                    options={ { responsive: "simple" } }
+                />
+            </div>
+        </MuiThemeProvider>
     );
 };

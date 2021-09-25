467b7a3077d8d5441115e8246abcf1d9fb044c7d
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-09-25 00:07:47 -0300

    refactor: adicionado uso da tabela generica

--- frontend/src/pages/category/components/Table.tsx
+++ frontend/src/pages/category/components/Table.tsx
@@ -1,54 +1,77 @@
-import React, {useEffect, useState} from 'react';
-import MUIDataTable, {MUIDataTableColumn} from "mui-datatables";
-import {format, parseISO} from 'date-fns';
+import React, { useEffect, useState } from 'react';
+import { format, parseISO } from 'date-fns';
 
-import {categoryHttp} from "../../../util/http/category-http";
-import {BadgeNo, BadgeYes} from "../../../components";
-import {Category} from "../../../core/models";
+import { categoryHttp } from "../../../util/http/category-http";
+import { BadgeNo, BadgeYes, Table as MyTable, TableColumn } from "../../../components";
+import { Category } from "../../../core/models";
+import { useSnackbar } from "notistack";
 
-const columnsDefinition: MUIDataTableColumn[] = [
+const columnsDefinition: TableColumn[] = [
+    {
+        name: 'id',
+        label: 'ID',
+        options: {
+            sort: false
+        }
+    },
     {
         name: "name",
-        label: "Nome"
+        label: "Nome",
     },
     {
         name: "is_active",
         label: "Ativo ?",
         options: {
-            customBodyRender: (value) => {
+            customBodyRender: ( value ) => {
                 return value
-                    ? <BadgeYes />
-                    : <BadgeNo />;
-            }
+                    ? <BadgeYes/>
+                    : <BadgeNo/>;
+            },
         }
     },
     {
         name: "created_at",
         label: "Criado em",
         options: {
-            customBodyRender: (value) => {
-                return <span>{ format(parseISO(value), 'dd/MM/yyyy HH:ii') }</span>
+            customBodyRender: ( value ) => {
+                return <span>{ format( parseISO( value ), 'dd/MM/yyyy HH:ii' ) }</span>
             }
         }
+    },
+    {
+        name: "actions",
+        label: "Ações",
     }
 ];
 
 export const Table = () => {
-    const [ data, setData ] = useState<Category[]>([]);
+    const snackbar = useSnackbar();
+    const [ loading, setLoading ] = useState( false );
+    const [ data, setData ] = useState<Category[]>( [] );
 
-    useEffect(() => {
+    useEffect( () => {
+        setLoading( true )
         categoryHttp.list()
-            .then((response) =>
-                setData(response.data.data)
+            .then( ( response ) =>
+                setData( response.data.data )
             )
-    }, []);
+            .catch( ( err ) => {
+                snackbar.enqueueSnackbar(
+                    err.message,
+                    { variant: "error" }
+                );
+            } )
+            .finally( () => setLoading( false ) )
+    }, [ snackbar ] );
 
     return (
         <div>
-            <MUIDataTable
+            <MyTable
                 columns={ columnsDefinition }
                 data={ data }
                 title={ 'Listagem de categorias' }
+                isLoading={ loading }
+                options={ { responsive: "simple" } }
             />
         </div>
     );
